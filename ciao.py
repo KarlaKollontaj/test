68d6baa445ff9dd3ada857b504d2203c100035de
KarlaKollontaj <carlavaleriabarnab@gmail.com>
2023-03-22 09:25:38 +0100

    gitignore

--- ciao.py
+++ ciao.py
@@ -1,1 +1,9 @@
-#ciao ciao ciao
+def numero_positivo_negativo(num):
+    while x != "s":
+        if num>0:
+            print('il numero è positivo')
+        elif num<0:
+            print('il numero è negativo')
+        else:
+            print('il numero è 0')
+
